3a922bd9d71b010c5a305b6c448bafbe5ec4669e
rkwebvishal <163818759+rkwebvishal@users.noreply.github.com>
2024-12-30 19:08:48 +0530

    login page done

---
+++ resources/views/master.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>E-Comm project</title>
+</head>
+
+<body>
+
+    {{View::make('header')}}
+    @yield('content')
+    {{View::make('footer')}}
+</body>
+
+<style>
+    .custom-login{
+        height: 500px;
+        padding-top: 100px;
+    }
+
+    /* .footer{
+        margin-bottom: 0px;
+    } */
+
+</style>
+
+</html>
